cb24180af0c1b6feae0da2f358e8c89909ddc338
Khaidem Sandip Singha <khaidemsandipsingha@Khaidems-MacBook-Air.local>
2022-03-27 22:40:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Layout from './components/layout';
 import YoutubeLinks from './components/youtube';
+import HeadingTitle from './components/header';
 
 function App() {
   return (
     <div className="App">
       <Layout>
+        <HeadingTitle />
         <YoutubeLinks />
       </Layout>
     </div>
---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeadingTitle = () => {
+  return (
+    <div className="p-2 text-center">
+      Video messages from Dr Ishak Ahmed During 2021 to 2022
+    </div>
+  );
+};
+
+export default HeadingTitle;
